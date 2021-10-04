261da09819f92a09150e38992c0401cf84c8da4a
jahi1998 <13691554018@163.com>
2021-10-03 21:36:38 -0500

    add data

--- src/utilities/times.js
+++ src/utilities/times.js
@@ -12,7 +12,7 @@ export const terms = { F: "Fall", W: "Winter", S: "Spring" };
 const meetsPat =
   /^ *((?:M|Tu|W|Th|F)+) +(\d\d?):(\d\d) *[ -] *(\d\d?):(\d\d) *$/;
 
-const timeParts = (meets) => {
+export const timeParts = (meets) => {
   const [match, days, hh1, mm1, hh2, mm2] = meetsPat.exec(meets) || [];
   return !match
     ? {}
